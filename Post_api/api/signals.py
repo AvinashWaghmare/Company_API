30a9ae6121b1e48276885539a9375d66b55cd26b
AvinashWaghmare <49596012+AvinashWaghmare@users.noreply.github.com>
2023-07-30 17:01:53 +0530

    Update signals.py

--- Post_api/api/signals.py
+++ Post_api/api/signals.py
@@ -7,7 +7,7 @@ from .models import Post
 def send_post_creation_notification(sender, instance, created, **kwargs):
     if created:
         subject = 'New Post Created'
-        message = f'A new post "{instance.title}" has been created.'
+        message = f'Hello "{instance.author}", \n\nYour new post "{instance.title}" has been created.'
         from_email = 'aw422360@gmail.com'
         recipient_list = [instance.author.email]  # Replace with actual recipient email addresses
         send_mail(subject, message, from_email, recipient_list)
