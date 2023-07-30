fc7fef9d9294dfd567cf041c72237b58b3bf7f5f
AvinashWaghmare <49596012+AvinashWaghmare@users.noreply.github.com>
2023-07-30 13:06:09 +0530

    Update signals.py
    Email Changes

--- Post_api/api/signals.py
+++ Post_api/api/signals.py
@@ -8,6 +8,6 @@ def send_post_creation_notification(sender, instance, created, **kwargs):
     if created:
         subject = 'New Post Created'
         message = f'A new post "{instance.title}" has been created.'
-        from_email = 'your_email@example.com'
+        from_email = 'aw422360@gmail.com'
         recipient_list = ['recipient@example.com']  # Replace with actual recipient email address(es)
         send_mail(subject, message, from_email, recipient_list)
